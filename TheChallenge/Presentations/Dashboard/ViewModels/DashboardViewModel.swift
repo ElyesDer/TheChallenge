cd8274ada99fda2961b81d353f0370f64fed1525
Elyes <lyesderouich@gmail.com>
2022-05-26 11:43:11 +0200

    5 minutes commit for forgotten channel image on movie affiche

--- TheChallenge/Presentations/Dashboard/ViewModels/DashboardViewModel.swift
+++ TheChallenge/Presentations/Dashboard/ViewModels/DashboardViewModel.swift
@@ -30,32 +30,14 @@ class DashboardViewModel {
                 let content = try await self.synchronisation
                     .get()
                 DispatchQueue.main.async {
-                    self.structuredRowProvider = [StructureCellModel(row: content.type, content: content.contents)]
+                    self.structuredRowProvider = [
+                        StructureCellModel(row: content.type,
+                                           content: content.contents.filter{ $0.isInOffer } )
+                    ]
                 }
             } catch {
                 // error handling
             }
         }
-        
-        //        serviceProvider
-        //            .request(from: APIEndpoint(
-        //                method: .get,
-        //                endURL: .movies), of: ContentWrapper<[Content]>.self)
-        //            .map { content in
-        //                StructureCellModel(
-        //                    row: content.type,
-        //                    content: content.contents)
-        //            }
-        //            .sink { completion in
-        //                switch completion {
-        //                    case .finished : break
-        //                    case .failure(let error) :
-        //                        print(error)
-        //                }
-        //            } receiveValue: { structuredRow in
-        //                // use this because ContentWrapper<[Content]> is not an array
-        //                self.structuredRowProvider = [structuredRow]
-        //            }
-        //            .store(in: &cancellables)
     }
 }
